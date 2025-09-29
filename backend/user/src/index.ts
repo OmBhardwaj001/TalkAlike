504bcd909551846c9a77d179a4554b2879eff954
om bhardwaj <ombhardwaj155@gmail.com>
2025-09-29 14:41:57 +0530

    reddis mongo rabbitMQ added

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,10 +1,30 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+import {createClient} from 'redis'
+
+import userRoutes from './routes/user.js';
+import { connectRabbitMQ } from './config/rabbitmq.js';
+
+dotenv.config();
 
 
 const app = express();
 
-const port = 5000;
+const PORT = process.env.PORT;
 
-app.listen(port,()=>{
-    console.log(`server is running on port no: ${port}`);
+connectDb();
+connectRabbitMQ();
+
+export const redisClient = createClient({
+    url:process.env.REDIS_URL as string
+})
+
+redisClient.connect()
+.then(()=> console.log("connected to redis")).catch(console.error)
+
+app.use("/api/v1",userRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port no: ${PORT}`);
 })
