bbe931160fdd0d06b363001ead7ef1ba7f1ca758
om bhardwaj <ombhardwaj155@gmail.com>
2025-09-30 00:38:14 +0530

    added isAuth middleware

---
+++ backend/user/src/middlewares/isAuth.ts
@@ -0,0 +1,35 @@
+import type { IUser } from "../model/user.js";
+import type  { NextFunction, Request, Response } from 'express';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export interface AuthenticatedRequest extends Request{
+    user?: IUser | null ;
+}
+
+export const isAuth = async(req:AuthenticatedRequest, res:Response , next : NextFunction): Promise<void>=>{
+    try {
+        const authHeader = req.headers.authorization;
+        console.log(authHeader);
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            res.status(401).json({message:"please login - no auth header"});
+            return;
+        }
+
+        const token  = authHeader.split(" ")[1]
+        const decodedValue = jwt.verify(token as string ,process.env.JWT_SECRET as string) as JwtPayload
+
+        if(!decodedValue || !decodedValue.user){
+            res.status(401).json({message:"invalid token"});
+            return;
+        }
+
+        req.user = decodedValue.user;
+
+        next();
+    } catch (error) {
+        res.status(401).json({message:"please login - JWT error"});
+    }
+}
