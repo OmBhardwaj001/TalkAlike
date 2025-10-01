ae723cf642f6f15a96bd8f28fceb85e074e7a4c3
om bhardwaj <ombhardwaj155@gmail.com>
2025-10-01 14:21:20 +0530

    main index.ts

---
+++ backend/chat/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+import chatRoutes from './routes/chat.js'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+connectDb();
+
+app.use('/api/v1',chatRoutes);
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port no ${process.env.PORT}`)
+})
