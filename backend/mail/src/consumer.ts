23174ee59c87b82b47fee018983e046ab5e9eb3f
om bhardwaj <ombhardwaj155@gmail.com>
2025-10-24 22:27:03 +0530

    rabbitMQ retries

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -1,65 +1,74 @@
-import amqp from 'amqplib';
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const startSendOtpConsumer = async()=>{
+export const startSendOtpConsumer = async () => {
+  const MAX_RETRIES = 10;
+  const RETRY_DELAY = 5000;
+
+  let retries = 0;
+
+  while (retries < MAX_RETRIES) {
     try {
+      const connection = await amqp.connect({
+        protocol: "amqp",
+        hostname: process.env.RABBITMQ_HOST,
+        port: 5672,
+        username: process.env.RABBITMQ_USERNAME,
+        password: process.env.RABBITMQ_PASSWORD,
+      });
 
-        const connection = await amqp.connect({
-            protocol:"amqp",
-            hostname: process.env.RABBITMQ_HOST,
-            port : 5672,
-            username: process.env.RABBITMQ_USERNAME,
-            password:process.env.RABBITMQ_PASSWORD,
-        });  
+      const channel = await connection.createChannel();
 
-        const channel = await connection.createChannel();
+      const queueName = "send-otp";
 
-        const queueName = "send-otp";
+      channel.assertQueue(queueName, { durable: true });
 
-        channel.assertQueue(queueName,{durable:true});
+      console.log(
+        "🐰 Mail service consumer started , listening for otp mailes"
+      );
 
-        console.log("🐰 Mail service consumer started , listening for otp mailes");
+      channel.consume(queueName, async (msg) => {
+        if (msg) {
+          try {
+            const { to, subject, body } = JSON.parse(msg.content.toString());
 
-        channel.consume(queueName, async(msg)=>{
-            if(msg){
-                try {
-                    const {to,subject,body} = JSON.parse(msg.content.toString());
+            const transporter = nodemailer.createTransport({
+              host: "smtp.resend.com",
+              port: 587,
+              secure: false,
+              auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+              },
+            });
 
-                    const transporter = nodemailer.createTransport({
-                        host:"smtp.resend.com",
-                        port:587,
-                        secure:false,
-                        auth:{
-                            user:process.env.SMTP_USER,
-                            pass:process.env.SMTP_PASSWORD,
-                        }
-                    });
+            await transporter.sendMail({
+              from: "TalkAlike <noreply@cryptdrive.in>",
+              to,
+              subject,
+              text: body,
+            });
 
-                    await transporter.sendMail({
-                        from:"TalkAlike <noreply@cryptdrive.in>",
-                        to,
-                        subject,
-                        text:body
-                    });
+            console.log(`OTP mail sent to ${to}`);
+            channel.ack(msg);
+          } catch (error) {
+            console.log("failed to send otp", error);
+          }
+        }
+      });
 
-                    console.log(`OTP mail sent to ${to}`);
-                    channel.ack(msg);
-                    
-                } catch (error){
-                    console.log("failed to send otp",error);
-                }
-            }
-            
-
-        })
-
-
+      break;
     } catch (error) {
-        console.log("failed to start rabbitmq consumer",error)
-        
+      retries++;
+      await new Promise((res) => setTimeout(res, RETRY_DELAY));
+      console.log("failed to start rabbitmq consumer", error);
     }
+  }
 
-}
+  if (retries === MAX_RETRIES) {
+    process.exit(1);
+  }
+};
